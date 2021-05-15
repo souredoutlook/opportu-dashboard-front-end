1075275475a214deace4df170b06c44dcf793d57
souredoutlook <souredoutlook@gmail.com>
2021-05-15 00:34:51 -0400

    initialize draggable component

---
+++ src/components/assessments/Draggable.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
+
+export default function Draggable(props) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: props.id,
+  });
+
+  const style = {
+    // Outputs `translate3d(x, y, 0)`
+    transform: CSS.Translate.toString(transform),
+    width: '10rem',
+  };
+
+  const onClick = (event)=> {
+    event.preventDefault(); 
+  }
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes} onClick={onClick}>
+      {props.children}
+    </button>
+  );
+};

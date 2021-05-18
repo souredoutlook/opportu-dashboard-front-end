9f69d1dac5a5725ee51b8f1957614274a02afee0
souredoutlook <souredoutlook@gmail.com>
2021-05-17 21:39:10 -0400

    add axios call for session validation

--- src/components/Assessments.jsx
+++ src/components/Assessments.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DndContext } from '@dnd-kit/core';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -7,12 +7,13 @@ import Draggable from './assessments/Draggable';
 import DroppableList from './assessments/DroppableList';
 
 import coreValues from '../helpers/coreValues'
-import { parseParents, initialState } from '../helpers/assessments'
+import { parseParents, initialState, constants } from '../helpers/assessments'
 
 import './Assessments.scss';
 
+const { FORBIDDEN, COMPLETE, LOADING, INCOMPLETE } = constants;
 export default function Assessments() {
-  
+  const [formState, setFormState] = useState(LOADING);
   const [parent, setParent] = useState(initialState);
 
   const draggableList = coreValues.map((value, index) => {
@@ -49,6 +50,23 @@ export default function Assessments() {
   
   const { id } = useParams();
 
+  useEffect(()=>{
+    axios.get(`/assessments/values/${id}`)
+    .then((response)=>{
+      if (response.status === 200) {
+        setFormState(INCOMPLETE);
+      }
+    })
+    .catch(err => {
+      const {status} = err.response;
+      if (status === 401) {
+        setFormState(COMPLETE);
+      } else if (status === 403) {
+        setFormState(FORBIDDEN);
+      }
+    })
+  },[])
+
   function submit() {
 
     axios
@@ -69,32 +87,39 @@ export default function Assessments() {
   return (
     <section className="assessment">
       <article className="assessment--card">
-        <h3>Core Values Assessment id #{id}</h3>
-        <div className="assessment--form">
-          <DndContext onDragEnd={handleDragEnd} >
-            <form className="assessment--form wrap droppable">
-              <DroppableList 
-                parent={parent}
-                draggableList={draggableList}
-                handleChange={handleChange}
-              />
-            </form>
-            <form className="assessment--form row wrap">
-              {draggableList.filter(element => {
-                for (const value in parent) {
-                  if (parent[value].draggable && parent[value].draggable.key === element.key) {
-                    return null 
-                  }
-                }
-                return element;
-              })}
-            </form>
-          </DndContext>
-        </div>
-        <button type="submit" onClick={submit}>Submit Assessment</button>
-        <div className="assessment--error">
-          {parent.error && <p>Something went wrong...</p>}
-        </div>
+        {formState === INCOMPLETE &&
+          (<>
+            <h3>Root Values Assessment</h3>
+            <div className="assessment--form">
+              <DndContext onDragEnd={handleDragEnd} >
+                <form className="assessment--form wrap droppable">
+                  <DroppableList 
+                    parent={parent}
+                    draggableList={draggableList}
+                    handleChange={handleChange}
+                  />
+                </form>
+                <form className="assessment--form row wrap">
+                  {draggableList.filter(element => {
+                    for (const value in parent) {
+                      if (parent[value].draggable && parent[value].draggable.key === element.key) {
+                        return null 
+                      }
+                    }
+                    return element;
+                  })}
+                </form>
+              </DndContext>
+            </div>
+            <button type="submit" onClick={submit}>Submit Assessment</button>
+            <div className="assessment--error">
+              {parent.error && <p>Something went wrong...</p>}
+            </div>
+          </>
+          ) 
+        }
+        {formState === COMPLETE && <h3>This assessment has been completed...</h3>}
+        {formState === FORBIDDEN && <h3>This assessment does not belong to your account...</h3>}
       </article>
     </section>
   );

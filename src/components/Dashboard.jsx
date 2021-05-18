59a7114f1a5499aa378bb736577decb2d9246e25
souredoutlook <souredoutlook@gmail.com>
2021-05-18 16:11:32 -0400

    refactor Dashboard with axios call and RootValues

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RootValues from './dashboard/RootValues';
+
+import axios from 'axios';
 
 import './Dashboard.scss';
 
 export default function Dashboard(props) {
 
+  const [dashData, setDashData] = useState({error: false});
+  
+  const { id } = props;
+
+  useEffect(()=>{
+    axios
+      .get(
+      `/users/${id}/assessments`
+    )
+    .then(response => {
+      if (response.status === 200) {
+        setDashData(prev => ({...prev, ...response.data}))
+      }
+    })
+    .catch(err => {
+      setDashData(prev => ({...prev, error: true}));
+    });
+  }, []);
+
+  const rootValues = dashData.assessments && dashData.assessments.core_values || undefined;
+
   return (
     <section className="dashboard">
-      <article className="dashboard--card">
-        <h3>Root Values</h3>
-      </article>
+      <RootValues rootValues={rootValues} />
       <article className="dashboard--card">
       </article>
       <article className="dashboard--card">

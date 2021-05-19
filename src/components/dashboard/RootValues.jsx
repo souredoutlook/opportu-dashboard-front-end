f02bf823b1b36d61c83a7afaff95fbb3173ab855
souredoutlook <souredoutlook@gmail.com>
2021-05-19 09:36:16 -0400

    refactor RootValues component

--- src/components/dashboard/RootValues.jsx
+++ src/components/dashboard/RootValues.jsx
@@ -1,57 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactWordcloud from 'react-wordcloud';
 
-import { parseValues } from '../../helpers/rootValues';
+import { parseValues, colors } from '../../helpers/rootValues';
 
 import '../Dashboard.scss';
 
 
-const words = [
-  {
-    text: 'told',
-    value: -1,
-  },
-  {
-    text: 'mistake',
-    value: -2,
-  },
-  {
-    text: 'thought',
-    value: -3,
-  },
-  {
-    text: 'bad',
-    value: -4,
-  },
-];
 
 const callbacks = {
-  getWordColor: word => word.value > 50 ? "blue" : "red",
-  onWordClick: console.log,
-  onWordMouseOver: console.log,
-  getWordTooltip: word => null,
+  // getWordColor: word => colors[-word.value], 
+  getWordTooltip: word => `#${-word.value}`,
 }
 
 const options = {
+  colors,
+  fontFamily: 'montserrat',
   rotations: 0,
-  rotationAngles: [-90, 0],
+  fontSizes: [30, 50],
+  scale: 'linear',
 };
 
-const size = [200, 100];
+const size = [300, 300];
+// 300, 300 is minimum size
+
 
 export default function RootValues(props) {
   
-  const { core_values } = props;
+  
+  const { rootValues } = props;
+  
+  const words = parseValues(rootValues, true);
+  
+  const [toggleState, setToggleState] = useState(true);
+
+  function handleClick() {
+    setToggleState(!toggleState);
+  };
 
   return (
-      <article className="dashboard--card">
+    <article className="dashboard--card">
         <h3>Root Values</h3>
-        <ReactWordcloud
-          callbacks={callbacks}
-          options={options}
-          size={size}
-          words={words}
-        />
-      </article>
+        {toggleState && 
+        <div style={{ height: '100%', width: '100%' }}>
+          <ReactWordcloud
+            callbacks={callbacks}
+            options={options}
+            words={words}
+          />
+        </div>}
+        {!toggleState && 
+        <div className="dashboard--list">
+            {parseValues(rootValues, false).map((element, index) => {
+              return (
+                <p>#{index+1} - {element}</p>
+              )
+            })}
+        </div>} 
+        <div className={`dashboard--toggle  ${toggleState ? 'left' : 'right'}`} onClick={handleClick} >
+          <div className='dashboard--slider' />
+        </div>
+    </article>
   );
 };

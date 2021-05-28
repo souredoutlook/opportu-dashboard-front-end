d8e156c2be96b4e2c5b164c57a805077c3047b59
souredoutlook <souredoutlook@gmail.com>
2021-05-27 23:24:08 -0400

    add placeholder text to Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-export default function Welcome(props) {
+import './Welcome.scss';
 
-  const loc = JSON.stringify(props.location);
+export default function Welcome() {
 
   return (
-    <div className="container">
-      <h3>Welcome Page</h3>
-      <p className="flow-text">{loc}</p>
+    //this is a placeholder
+    <div className="welcome">
+      <div className="welcome--card">
+        <h2>Coaching.</h2>
+        <h2>Training.</h2>
+        <h2>Innovation.</h2>
+        <div className="welcome--group">
+          <h3 className="blue">Yes, we cross the streams.</h3>
+          <p>Why? Because culture, leadership and success don’t manage themselves. <strong>Now more than ever.</strong></p>
+        </div>
+		  </div>
     </div>
   );
 };

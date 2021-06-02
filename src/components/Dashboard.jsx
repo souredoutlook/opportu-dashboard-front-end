89642038185a13b72fc12e2cb20481bf67dad6d7
souredoutlook <souredoutlook@gmail.com>
2021-06-01 22:38:22 -0400

    import RootValuesList and conditionally render

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import RootValues from './dashboard/RootValues';
+import RootValuesList from './dashboard/RootValuesList';
 import Facet5 from './dashboard/Facet5';
 
 import axios from 'axios';
 
+import { sortKeysByDate, formatValuesForWordCloud } from '../helpers/rootValues';
 import './Dashboard.scss';
 
 export default function Dashboard(props) {
 
   const [dashData, setDashData] = useState({error: false});
   
-  const { id } = props;
+  const { id, historicalRootValues } = props;
 
   useEffect(()=>{
     axios
@@ -31,8 +33,15 @@ export default function Dashboard(props) {
   
   return (
     <section className="dashboard">
-      {rootValues && <RootValues rootValues={rootValues} />}
-      <Facet5 dashData={dashData} setDashData={setDashData} />
+      {!historicalRootValues && 
+        <>
+          {rootValues && <RootValues rootValues={rootValues} assessmentKey={sortKeysByDate(rootValues)[0]} />}
+          <Facet5 dashData={dashData} setDashData={setDashData} />
+        </>
+      }
+      {historicalRootValues && 
+        rootValues && <RootValuesList rootValues={rootValues} keyList={sortKeysByDate(rootValues)}/>
+      }
     </section>
   );
 };
